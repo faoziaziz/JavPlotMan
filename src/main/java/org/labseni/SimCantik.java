1e66e4eac63317f7a0ec089746fcc9ff532775ab
Aziz Amerul Faozi <cahalapisan@gmail.com>
2018-04-10 20:56:07 +0700

    Update kelas baru

--- src/main/java/org/labseni/SimCantik.java
+++ src/main/java/org/labseni/SimCantik.java
@@ -10,23 +10,37 @@ import java.io.File;
 public class SimCantik {
 	public SimCantik()
 	{
-
+		
+	
 	}
+
+	private class bikinGambar 
+	{
+		
+		public void bikinGambar()
+		{
+			DefaultPieDataset pieDataset = new DefaultPieDataset();
+			pieDataset.setValue("A", new Integer(75));
+			pieDataset.setValue("B", new Integer(10));
+			pieDataset.setValue("C", new Integer(10));
+			pieDataset.setValue("D", new Integer(5));
+	
+			JFreeChart chart = ChartFactory.createPieChart("CSC408 Mark Distribution", pieDataset, true, true, false);
+			try {
+				ChartUtils.saveChartAsJPEG(new File("chasrt.jpg"), chart, 500, 300);
+			} 
+			catch (Exception e) 
+			{
+				System.out.println("Problem occurred creating chart."); 
+			}
+		}
+	}
+
 	public void Jalankan()
 	{
-		DefaultPieDataset pieDataset = new DefaultPieDataset();
-		pieDataset.setValue("A", new Integer(75));
-		pieDataset.setValue("B", new Integer(10));
-		pieDataset.setValue("C", new Integer(10));
-		pieDataset.setValue("D", new Integer(5));
-	
-		JFreeChart chart = ChartFactory.createPieChart("CSC408 Mark Distribution", //Title
-				pieDataset, true, true, false);
-	try {
-		ChartUtils.saveChartAsJPEG(new File("chart.jpg"), chart, 500, 300);
-	} catch (Exception e) {
-		System.out.println("Problem occurred creating chart."); }
-	
+		bikinGambar makan= new bikinGambar();
+		makan.bikinGambar();
+
 	}
 
 }
